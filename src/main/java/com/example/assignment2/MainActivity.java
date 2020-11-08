59b343192f6e8e9fe59bcf3fe2051fce6bbdfc1a
Osama Mafarjh <72619778+OsamaMafarjh@users.noreply.github.com>
2020-11-08 21:56:55 +0200

    saedAmro
    Signed-off-by: Osama Mafarjh <72619778+OsamaMafarjh@users.noreply.github.com>

--- src/main/java/com/example/assignment2/MainActivity.java
+++ src/main/java/com/example/assignment2/MainActivity.java
@@ -29,21 +29,40 @@ public class MainActivity extends AppCompatActivity {
 
     private void loadDate() {
         name = (EditText) findViewById(R.id.name);
+
         weight = (EditText) findViewById(R.id.weight);
+
         hight = (EditText) findViewById(R.id.hight);
+
+
         BMI = (EditText) findViewById(R.id.bmi);
+
         gender = (Spinner) findViewById(R.id.gender);
+
         BMI.setEnabled(false);
+
         SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+
         String getName = sharedPref.getString("name", "");
+
         long getWeight = sharedPref.getLong("weight", 0);
+
         long getBmi = sharedPref.getLong("bmi", 0);
+
         long getHight = sharedPref.getLong("hight", 0);
+
         int getGender = sharedPref.getInt("gender", 0);
+
+
+
         name.setText(getName);
+
         weight.setText(getWeight + "");
+
         hight.setText(getHight + "");
+
         BMI.setText(getBmi + "");
+
         gender.setSelection(getGender);
 
 
@@ -55,19 +74,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickSave(View view) {
+
         String names = name.getText().toString();
+
         double hights = Double.parseDouble(hight.getText().toString());
+
         double weights = Double.parseDouble(weight.getText().toString());
+
         double bmis = weights / Math.sqrt(hights);
+
+
         int genders = gender.getSelectedItemPosition();
         SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+
         SharedPreferences.Editor editor = sharedPref.edit();
+
         editor.putLong("hight", (long) hights);
+
         editor.putLong("weight", (long) weights);
         editor.putLong("bmi", (long) bmis);
+
         editor.putString("name", names);
+
         editor.putInt("gender", genders);
         BMI.setText(bmis + "");
+
         editor.commit();
     }
+
+
 }
+
